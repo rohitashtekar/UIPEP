be50946a069579958976e4135b309a89581d7443
rohitashtekar <rrashtekar@gmail.com>
2021-05-19 18:31:21 +0530

    Started a mini project - World with critters

--- UIPEP-W3/d3project.js
+++ UIPEP-W3/d3project.js
@@ -1,3 +1,15 @@
+var plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
 function Vector(x, y){
     this.x = x;
     this.y = y;
@@ -6,3 +18,72 @@ function Vector(x, y){
 Vector.prototype.plus = function(other){
     return new Vector(this.x + other.x, this.y + other.y);
 };
+
+var grid = ["top left", "top middle", "top right", "bottom left", "bottom middle", "bottom right"];
+
+function Grid(width, height){
+    this.space = new Array(width * height);
+    this.width = width;
+    this.height = height;
+}
+
+Grid.prototype.isInside = function(vector){
+    return vector.x >= 0 && vector.y < this.width &&
+    vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function(vector){
+    return this.space[vector.x + vector.y * this.width];
+};
+Grid.prototype.set = function(vector, value){
+    this.space[vector.x + vector.y * this.width] = value;
+};
+
+var directions = {
+    "n":  new Vector( 0, -1),
+    "ne": new Vector( 1, -1),
+    "e":  new Vector( 1,  0),
+    "se": new Vector( 1,  1),
+    "s":  new Vector( 0,  1),
+    "sw": new Vector(-1,  1),
+    "w":  new Vector(-1,  0),
+    "nw": new Vector(-1, -1)
+};
+
+function elementFromChar(legend, ch) {
+    if (ch == " ")
+        return null;
+    var element = new legend[ch]();
+    element.originChar = ch;
+    return element;
+}
+  
+function World(map, legend) {
+    var grid = new Grid(map[0].length, map.length);
+    this.grid = grid;
+    this.legend = legend;
+
+    map.forEach(function(line, y) {
+        for (var x = 0; x < line.length; x++)
+        grid.set(new Vector(x, y),
+                    elementFromChar(legend, line[x]));
+    });
+}
+
+function charFromElement(element) {
+    if (element == null)
+      return " ";
+    else
+      return element.originChar;
+  }
+  
+  World.prototype.toString = function() {
+    var output = "";
+    for (var y = 0; y < this.grid.height; y++) {
+      for (var x = 0; x < this.grid.width; x++) {
+        var element = this.grid.get(new Vector(x, y));
+        output += charFromElement(element);
+      }
+      output += "\n";
+    }
+    return output;
+  };
