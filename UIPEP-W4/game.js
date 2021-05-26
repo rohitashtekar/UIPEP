ffc3ffba2b5647d19412fc0e73e6bf7d466ff32a
rohitashtekar <rrashtekar@gmail.com>
2021-05-26 19:48:23 +0530

    Added JS to the game

---
+++ UIPEP-W4/game.js
@@ -0,0 +1,80 @@
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4 ,5],
+    [6, 7, 8]
+];
+
+const cellElements = document.querySelectorAll('.cell');
+let winMessageElement = document.querySelector('.winning-text');
+let winMessageText = document.querySelector('[data-winning-text]');
+const restartBtn = document.querySelector('.btn');
+let oTurn;
+
+startGame();
+
+function clickHandle(event) {
+    const cell = event.target;
+    const currentClass =  oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass)
+    if(checkWin(currentClass)) {
+        endGame(false);
+    }
+    else if(isDraw()){
+        endGame(true)
+    }
+    else {
+        switchTurn();
+    }
+}
+
+function endGame(draw) {
+    if(draw) {
+        winMessageText.innerText = `Draw!`;
+    }
+    else {
+        winMessageText.innerText = `Team ${oTurn ? "O" : "X"} Wins`;
+    }
+    winMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || 
+        cell.classList.contains(O_CLASS)
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function switchTurn() {
+    oTurn = !oTurn;
+}
+
+restartBtn.addEventListener('click', startGame)
+
+function startGame() {
+    oTurn = false;
+    cellElements.forEach( cell =>{
+        cell.addEventListener('click', clickHandle, {once : true})
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+    });
+    winMessageElement.classList.remove('show');
+}
+
+function checkWin(currentClass) {
+    return WIN_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        })
+    })
+}
