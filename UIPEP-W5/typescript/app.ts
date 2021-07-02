3b6d07bb3da6cf4cb63e27cf9476d2858c5bb272
rohitashtekar <rrashtekar@gmail.com>
2021-07-02 15:37:47 +0530

    Update

--- UIPEP-W5/typescript/app.ts
+++ UIPEP-W5/typescript/app.ts
@@ -1,6 +1,27 @@
-function add(num1: number,num2: number) {
-    return num1 + num2;
-}
+// function add(num1: number,num2: number) {
+//     return num1 + num2;
+// }
 
-let res = add(10, 12);
-console.log(res);
+// let res = add(10, 12);
+// console.log(res);
+
+// const obj: {
+//     name: string;
+//     age: number;
+// } = {
+const obj = {
+    name: "Rohit",
+    age: 23,
+    hobbies: [`Sports`,`Cooking`,`Football`]
+};
+
+let favActivities: string[];
+favActivities = ["Cycling","Working out"];
+
+console.log(obj.hobbies);
+
+for(let hobby of obj.hobbies){
+    console.log(hobby.toUpperCase());
+}
+obj.hobbies.push(`Reading`);
+console.log(obj);
